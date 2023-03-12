2fba0ff2f1169bc3d4522be5fdd8b8a914a6a261
mohitbasnet08 <118841933+Mohitbasnet@users.noreply.github.com>
2023-03-12 15:44:26 +0530

    Add files via upload

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request,flash,redirect,url_for
-import pymysql.connections
+from flask import Flask, render_template, request, redirect, url_for, flash
+import pymysql.cursors
 
+# Connect to the database
 # pip install pymysql
 # pip install cryptography
 # create database named "crud"
@@ -11,45 +12,66 @@ connection = pymysql.connect(host='localhost',
                              database='crud',
                              )
 
+
 app = Flask(__name__)
 
+# by defaul get request aaauxa
 @app.route('/')
 def Index():
-    cur = connection.cursor()
-    cur.execute("SELECT  * FROM students")
-    data = cur.fetchall()
+    cur = connection.cursor() #cur vanne instance banako
+    cur.execute("SELECT  * FROM student")
+    data = cur.fetchall() # jj return vako data xa yo data ma aayera basxa
     cur.close()
 
-    return render_template('index.html', students=data)
+    return render_template('index.html', student=data)
 
 
 @app.route('/insert', methods=['POST'])
 def insert():
-
+#  data secure garna lai usually post request pathaine garinxa
     if request.method == "POST":
-        flash("Data Inserted Successfully")
+        flash("Data Inserted Successfully") #jaba halme data insert garxau euta pop up aauxa
         name = request.form['name']
         email = request.form['email']
         phone = request.form['phone']
         cur = connection.cursor()
         cur.execute(
-            "INSERT INTO students (name, email, phone) VALUES (%s, %s, %s)", (name, email, phone))
+            "INSERT INTO student (name, email, phone) VALUES (%s, %s, %s)", (name, email, phone))
         connection.commit()
-        return redirect(url_for('Index'))
+        return redirect(url_for('Index')) #url_for le index.py lai aajai call garxa
 
 
-@app.route('/delete/<string:id_data>', methods=['GET'])
+@app.route('/delete/<string:id_data>', methods=['GET']) # string : id data le chai data ma j xa tellai delate 
 def delete(id_data):
-    return render_template('index.html')
+    flash("Record Has Been Deleted Successfully")
+    cur = connection.cursor()
+    cur.execute("DELETE FROM student WHERE id=%s", (id_data,))
+    connection.commit()
+    return redirect(url_for('Index'))  
 
 
 @app.route('/update', methods=['POST', 'GET'])
 def update():
+
     if request.method == 'POST':
-        return render_template('index.html')
+        id_data = request.form['id'] 
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        cur = connection.cursor()
+        cur.execute("""
+               UPDATE student
+               SET name=%s, email=%s, phone=%s
+               WHERE id=%s 
+            """, (name, email, phone, id_data))
+        flash("Data Updated Successfully")
+        connection.commit()
+        return redirect(url_for('Index'))
 
 
-if __name__== '__main__':
+if __name__ == '__main__':
+    # session cookies for protection against cookie data tampering.
     app.secret_key = 'super secret key'
+    # It will store in the hard drive
     app.config['SESSION_TYPE'] = 'filesystem'
     app.run(debug=True)
